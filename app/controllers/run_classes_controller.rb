bd8aef14ee8df1e1a07bd8a781596d3e54b6f1ec
Oumaima Azzat <oumaimazzat@gmail.com>
2021-01-05 17:58:05 -0500

    tuesdayevening

--- app/controllers/run_classes_controller.rb
+++ app/controllers/run_classes_controller.rb
@@ -1,2 +1,10 @@
 class RunClassesController < ApplicationController
+
+    def index
+        @run_classes = RunClass.all 
+    end 
+
+    def show
+        @run_class = RunClass.find(params[:id])
+    end 
 end
