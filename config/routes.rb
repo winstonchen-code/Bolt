70efec0f275d211aafe97f2ae1279ccb04170201
Winston Chen <winstonchen.wm@gmail.com>
2021-01-06 10:31:58 -0500

    third update

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new', as: 'login'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
+
+  get "/homepage", to: "static#homepage"
   
 end
